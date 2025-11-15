f81fec70261e309f777ffde3a33592a07fd30dc1
MilkteaRabbitt <natulink.ichiko+git@gmail.com>
2025-11-15 11:21:25 +0900

    feat: サイドバーのラジオボタン表示を改善し、初期AIメッセージの表示方法を更新

--- components.py
+++ components.py
@@ -23,15 +23,16 @@ def display_app_title():
 
 def display_select_mode():
     """
-    回答モードのラジオボタンを表示（サイドバーに表示）
+    回答モードのラジオボタンを表示(サイドバーに表示)
     """
-with st.sidebar:
+    with st.sidebar:
         st.markdown("## 利用目的")
         # 回答モードを選択する用のラジオボタンを表示
         st.session_state.mode = st.radio(
             label="利用目的",
             options=[ct.ANSWER_MODE_1, ct.ANSWER_MODE_2],
-            label_visibility="hidden"
+            label_visibility="hidden",
+            key="select_mode_radio" # radioボタンの状態をセッション間で保持するためのキー指定
         )
         
         # 区切り線
@@ -54,22 +55,12 @@ def display_initial_ai_message():
     AIメッセージの初期表示
     """
     with st.chat_message("assistant"):
-        # 「st.success()」とすると緑枠で表示される
-        st.markdown("こんにちは。私は社内文書の情報をもとに回答する生成AIチャットボットです。上記で利用目的を選択し、画面下部のチャット欄からメッセージを送信してください。")
-"""
-        # 「社内文書検索」の機能説明
-        st.markdown("**【「社内文書検索」を選択した場合】**")
-        # 「st.info()」を使うと青枠で表示される
-        st.info("入力内容と関連性が高い社内文書のありかを検索できます。")
-        # 「st.code()」を使うとコードブロックの装飾で表示される
-        # 「wrap_lines=True」で折り返し設定、「language=None」で非装飾とする
-        st.code("【入力例】\n社員の育成方針に関するMTGの議事録", wrap_lines=True, language=None)
+        # 緑枠で表示
+        st.success("こんにちは。私は社内文書の情報をもとに回答する生成AIチャットボットです。サイドバーで利用目的を選択し、画面下部のチャット欄からメッセージを送信してください。")
+        
+        # 黄色枠で表示
+        st.warning("⚠️ 具体的に入力したほうが期待通りの回答を得やすいです")
 
-        # 「社内問い合わせ」の機能説明
-        st.markdown("**【「社内問い合わせ」を選択した場合】**")
-        st.info("質問・要望に対して、社内文書の情報をもとに回答を得られます。")
-        st.code("【入力例】\n人事部に所属している従業員情報を一覧化して", wrap_lines=True, language=None)
-"""
 
 def display_conversation_log():
     """
